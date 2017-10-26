e9b43751f93c19bacc5eb0cfce9bd7e414631310
rputman0 <rputman0@gmail.com>
2017-10-26 14:25:43 -0400

    Update SimonSays.py
    -add functions
    -remove duplicate code
    -remove comments

--- SimonSays.py
+++ SimonSays.py
@@ -1,3 +1,4 @@
+from random import *
 import pygame
 
 #instanciate a new window
@@ -19,14 +20,6 @@ lightRed = [253,160,122]
 lightGreen = [34,139,34]
 lightYellow = [227,250,96]
 
-#create Four Squares (red,green,blue,yellow)
-pygame.draw.rect(screen, red, [0,0,150,150],0)
-pygame.draw.rect(screen, green, [0,150,150,150],0)
-pygame.draw.rect(screen, blue, [149,0,150,150],0)
-pygame.draw.rect(screen, yellow, [150,150,150,150],0)
-
-pygame.display.update()
-
 colorSequence = list()
 def getSequence(color,colorSequence):
     if(color == lightBlue):
@@ -38,7 +31,41 @@ def getSequence(color,colorSequence):
     elif(color == lightYellow):
         colorSequence.append("YELLOW")
 
-#CLOSING THE WINDOW, Place events here
+TOPLEFT = [0,0,150,150]
+TOPRIGHT = [149,0,150,150]
+BOTLEFT = [0,150,150,150]
+BOTRIGHT = [150,150,150,150]
+
+def drawSquare(color,position):
+    pygame.draw.rect(screen,color,position,0)
+    pygame.display.update()
+    getSequence(color,colorSequence)
+
+def drawDefault():
+    pygame.draw.rect(screen, red, TOPLEFT,0)
+    pygame.draw.rect(screen, blue, TOPRIGHT,0)
+    pygame.draw.rect(screen, green, BOTLEFT,0)
+    pygame.draw.rect(screen, yellow, BOTRIGHT,0)
+
+def userClick():
+    mousePos = pygame.mouse.get_pos()
+    if(event.type == pygame.MOUSEBUTTONDOWN and (mousePos[0] > 0 and mousePos[0] < 150 and mousePos[1] < 150)):
+        drawSquare(lightRed,TOPLEFT)
+    elif(event.type == pygame.MOUSEBUTTONDOWN and (mousePos[0] > 150 and mousePos[0] < 300 and mousePos[1] < 150)):
+        drawSquare(lightBlue,TOPRIGHT)
+    elif(event.type == pygame.MOUSEBUTTONDOWN and (mousePos[0] > 0 and mousePos[0] < 150 and mousePos[1] > 150 and mousePos[1] < 300)):
+        drawSquare(lightGreen,BOTLEFT)
+    elif(event.type == pygame.MOUSEBUTTONDOWN and (mousePos[0] > 150 and mousePos[0] < 300 and mousePos[1] > 150 and mousePos[1] < 300)):
+        drawSquare(lightYellow,BOTRIGHT)
+        getSequence(lightYellow,colorSequence)
+    else:
+        drawDefault()
+        pygame.display.update()
+
+drawDefault()
+pygame.display.update()
+
+#CLOSING THE WINDOW (this is the main loop)
 running = True
 while running:
     for event in pygame.event.get():
@@ -46,40 +73,34 @@ while running:
             running = False
 
         #let the user click on the buttons
-        mousePos = pygame.mouse.get_pos()
-        if(event.type == pygame.MOUSEBUTTONDOWN and (mousePos[0] > 0 and mousePos[0] < 150 and mousePos[1] < 150)):
-            pygame.draw.rect(screen, lightRed, [0,0,150,150],0)
-            getSequence(lightRed,colorSequence)
-            pygame.display.update()
-        elif(event.type == pygame.MOUSEBUTTONDOWN and (mousePos[0] > 150 and mousePos[0] < 300 and mousePos[1] < 150)):
-            pygame.draw.rect(screen, lightBlue, [149,0,150,150],0)
-            getSequence(lightBlue,colorSequence)
-            pygame.display.update()
-        elif(event.type == pygame.MOUSEBUTTONDOWN and (mousePos[0] > 0 and mousePos[0] < 150 and mousePos[1] > 150 and mousePos[1] < 300)):
-            pygame.draw.rect(screen, lightGreen, [0,150,150,150],0)
-            getSequence(lightGreen,colorSequence)
-            pygame.display.update()
-        elif(event.type == pygame.MOUSEBUTTONDOWN and (mousePos[0] > 150 and mousePos[0] < 300 and mousePos[1] > 150 and mousePos[1] < 300)):
-            pygame.draw.rect(screen, lightYellow, [150,150,150,150],0)
-            getSequence(lightYellow,colorSequence)
-            pygame.display.update()
-        else:
-            pygame.draw.rect(screen, red, [0,0,150,150],0)
-            pygame.draw.rect(screen, green, [0,150,150,150],0)
-            pygame.draw.rect(screen, blue, [149,0,150,150],0)
-            pygame.draw.rect(screen, yellow, [150,150,150,150],0)
-            pygame.display.update()
+        userClick()
 
         #press enter to start the game
+        randomColor = [lightBlue,lightGreen,lightYellow,lightRed]
+        squareLocation =[TOPRIGHT,BOTLEFT,BOTRIGHT,TOPLEFT]
         if(event.type == pygame.KEYDOWN):
-           if(event.key == pygame.K_RETURN):
-               #print(colorSequence)
-               #6. Create random sequence of red,green,blue,yellow (1 = red, ...,4 = yellow)
-                    #6.A Store random sequence into a list
-                    #6.B Use for loop to display list on screen
-                    #6.C For each item, turn square into lighter color
-                    #6.D Wait 1 Second, turn square back into darker color
-                    #6.E Go to next item in list
+           if(event.key == pygame.K_t): #use for test mode
+               print(colorSequence)
+           elif(event.key == pygame.K_RETURN):
+               end = 1
+               i = 0
+               score = 0
+               
+               for i in range(i,end): #choose this for one loop
+               #while(i <= end):  #to repeat
+                   square = randint(0,3)
+                   drawSquare(randomColor[square],squareLocation[square])
+                   
+                   pygame.time.wait(1000)
+
+                   checkAgainst = colorSequence
+                   print(checkAgainst)
+                   colorSequence = list()
+
+                   #wait for user input
+                   userClick()
+
+                   print(colorSequence)
                
         #7. Check if user enters the same list (clicks on squares in the same sequence)
 
